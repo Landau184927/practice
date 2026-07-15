15224e18b84f4e7085ee48187a0f360e67ba1c16
Zack <JeongEuichan184927@gmail.com>
2026-07-15 23:26:18 +0900

    3

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-print("This is Test Script.")
+# Module Decaration:
+import pandas
+import matplotlib.pyplot                                   as plt
+from sklearn.metrics         import accuracy_score         as acc
+from sklearn.model_selection import train_test_split       as tts
+from sklearn.model_selection import cross_val_score        as cvs
+from sklearn.neighbors       import KNeighborsClassifier   as KNC
+from sklearn.tree            import DecisionTreeClassifier as DTC
+from sklearn.ensemble        import RandomForestClassifier as RFC
+
+
+class Basic:
+    def __init__(self, URL):
+        self.data = pandas.read_csv(URL, index_col = 'Index')
+
+    def _initialize_(self):
+        self.extractDataFile()
+
+    def extractDataFile(self):
+        self.data
+        
+
+
+
+
+
+if __name__ == "__main__":
+    URL = "https://raw.githubusercontent.com/chriswmann/datasets/master/500_Person_Gender_Height_Weight_Index.csv"
+    Basic._initialize_(URL)
